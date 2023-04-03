40fe1b506d1b67a19da530e1dd519bd6baa9fc23
Richard Rauser <richard.rauser@gmail.com>
2023-04-03 07:39:40 +0000

    Updates to about page with links to repos.

--- pages/about.tsx
+++ pages/about.tsx
@@ -28,32 +28,32 @@ const About: NextPage = () => {
           </li>
           <li>
             <a target="_blank" href="https://thirdweb.com/solutions/minting">Thirdweb Minting</a> used for codeless deployment and management of:
-              <ol>
-                <li>
-                  the Circlez Membership contract (ERC-721) allowing users to claim unique, customised one-of-one membership NFTs
-                </li>
-                <li>
-              Circlez Membership Achievements (ERC-1555) allowing users to collect badges from multi-edition NFTs representing unlocked achievements that earn rewards.
-                </li>
-              </ol>
+            <ol>
+              <li>
+                the Circlez Membership contract (ERC-721) allowing users to claim unique, customised one-of-one membership NFTs
+              </li>
+              <li>
+                Circlez Membership Achievements (ERC-1555) allowing users to collect badges from multi-edition NFTs representing unlocked achievements that earn rewards.
+              </li>
+            </ol>
           </li>
           <li>
-            <a target="_blank" href="https://www.shopify.com/">Shopify</a> for build, management and hosting of the Circlez Coffee online store. 
+            <a target="_blank" href="https://www.shopify.com/">Shopify</a> for build, management and hosting of the Circlez Coffee online store.
           </li>
           <li>
-            <a target="_blank" href="https://shopify.dev/docs/apps">Shopify Apps</a> for extensions that unlock token gated discounts and merchandise in the Circlez Coffee Shopify web store. 
+            <a target="_blank" href="https://shopify.dev/docs/apps">Shopify Apps</a> for extensions that unlock token gated discounts and merchandise in the Circlez Coffee Shopify web store.
           </li>
           <li>
-            <a target="_blank" href="https://shopify.dev/docs/api/blockchain">Shopify Blockchain components</a> for connecting a wallet and gating discounts and merch in Shopify apps. 
+            <a target="_blank" href="https://shopify.dev/docs/api/blockchain">Shopify Blockchain components</a> for connecting a wallet and gating discounts and merch in Shopify apps.
           </li>
           <li>
-            <a target="_blank" href="https://shopify.dev/docs/api/admin-graphql/unstable/objects/GateConfiguration">Shopify Gates API</a> for for build and management of token gates. 
+            <a target="_blank" href="https://shopify.dev/docs/api/admin-graphql/unstable/objects/GateConfiguration">Shopify Gates API</a> for for build and management of token gates.
           </li>
           <li>
             <a target="_blank" href="https://www.npmjs.com/package/@shopify/shopify-api">shopify/shopify-api Node SDK</a> added for interacting with Shopify.
           </li>
           <li>
-            <a target="_blank" href="https://www.polygon.com/">Polygon</a> Mumbai testnet for the blockchain containing the membership and achievement NFTS. 
+            <a target="_blank" href="https://www.polygon.com/">Polygon</a> Mumbai testnet for the blockchain containing the membership and achievement NFTS.
           </li>
           <li>
             <a target="_blank" href="https://www.npmjs.com/package/@thirdweb-dev/sdk">ThirdWeb Node SDK</a> for blockchain support.
@@ -66,6 +66,20 @@ const About: NextPage = () => {
           </li>
         </ul>
       </div>
+      <div><h2>Source code</h2>
+        <ul>
+          <li>
+            The Shopify token gating app and extensions can be found <a target="_blank" href="https://github.com/richardrauser/circlez-coffee-shopify">on Github</a>.
+          </li>
+          <li>
+            The Circlez Coffee website and backend can be found <a target="_blank" href="https://replit.com/@RichardRauser/Circlez-Coffee-Website">in Replit</a>.
+          </li>
+          <li>
+            View the README.md files in those repos in order to build something like this yourself!
+          </li>
+        </ul>
+      </div>
+
     </div>
 
   )
